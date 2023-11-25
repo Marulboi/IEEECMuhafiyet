a5c82d905ea88936993b7a0d4c6befb52f2163f6
Marulboi <emiregenemutlu@gmail.com>
2023-11-25 11:31:27 +0300

    commmited

--- main.c
+++ main.c
@@ -6,6 +6,9 @@
 #define RAYGUI_IMPLEMENTATION
 #include "include/raygui.h"
 
+#define WINDOW_X 1800
+#define WINDOW_Y 900
+
 typedef struct Circle{
     Color color;
     float length;
@@ -26,16 +29,23 @@ typedef struct node {
 
 void appEnd(node **tail_t,Circle* data);
 void delEnd(node **tail);
+void addRandCircle(node** tail);
 Circle* newCricle(Color color, float size,float length, int x,int y,float speed);
 
 int main(){
 
-//Color clrpicked;
+Color clrpicked;
+float Nsize = 10.f;
+float Nlength = 10.f;
+float Nspeed = 3.f;
 float zoomMult = 1.f;
+float zoomMultF = 100.f;
+
+
 
 srand(time(NULL));
 
-InitWindow(1250, 800, "I SPIN YOU RIGHT ROUND BABY RIGHT ROUND");
+InitWindow(WINDOW_X, WINDOW_Y, "I SPIN YOU RIGHT ROUND BABY RIGHT ROUND");
 SetTargetFPS(60);
 float angle = 0.0f;
 node * head = (node *)malloc(sizeof(node));
@@ -48,6 +58,37 @@ head->circle = newCricle((Color){170,170,170,255},4,0,GetScreenWidth()/2,GetScre
 
     while (!WindowShouldClose())
     {   
+
+        GuiColorPicker((Rectangle){20,10,110,110},"select color",&clrpicked);
+        
+        GuiSliderBar((Rectangle){20,GetScreenHeight() - 30,110,20},"","Zoom",&zoomMultF,1.f,300.f);
+        GuiSliderBar((Rectangle){20,130,110,20},"","Size",&Nsize,5.f,50.f);
+        GuiSliderBar((Rectangle){20,160,110,20},"","Rod Length",&Nlength,5.f,50.f);
+        GuiSliderBar((Rectangle){20,190,110,20},"","Speed",&Nspeed,-30.f,30.f);
+        if(GuiButton((Rectangle){20,220,130,20},"Add Circle")){
+            appEnd(&tail,newCricle(clrpicked,Nsize,Nlength,tail->circle->x,tail->circle->y,Nspeed));
+        }
+        if(GuiButton((Rectangle){20,250,130,20},"Add Random Circle")){
+            addRandCircle(&tail);
+            }
+        if(GuiButton((Rectangle){20,280,130,20},"Remove Circle")){
+            delEnd(&tail);
+        }
+        if(GuiButton((Rectangle){20,310,130,20},"Add 5 Random Circles")){
+            for(int i = 0; i<5;i++){
+            addRandCircle(&tail);
+            }
+        }
+
+        if(GuiButton((Rectangle){20,340,130,20},"Remove 5 Circles")){
+            for(int i = 0; i<5;i++){
+                if(tail != head){
+                delEnd(&tail);
+                }
+            }
+        }
+
+        zoomMult = zoomMultF/100;
         if(angle>=360.0f){
             angle = angle + 359.998f;
         }
@@ -55,9 +96,10 @@ head->circle = newCricle((Color){170,170,170,255},4,0,GetScreenWidth()/2,GetScre
             angle += 0.002f;
         }
         current = head;
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
-
+        
         DrawCircle(current->circle->x,current->circle->y,current->circle->size,current->circle->color);
 
             if(current->next != NULL){
@@ -68,8 +110,8 @@ head->circle = newCricle((Color){170,170,170,255},4,0,GetScreenWidth()/2,GetScre
 
         while(current != NULL){
         
-            double endx = current->circle->length*sin(angle*current->circle->speed)*zoomMult;
-            double endy = current->circle->length*cos(angle*current->circle->speed)*zoomMult;
+            double endx = (current->circle->length + current->circle->size)*sin(angle*current->circle->speed)*zoomMult;
+            double endy = (current->circle->length + current->circle->size)*cos(angle*current->circle->speed)*zoomMult;
             
             DrawCircle(current->circle->x, current->circle->y, current->circle->size*zoomMult + 2, BLACK);
             DrawCircle(current->circle->x, current->circle->y, current->circle->size*zoomMult, current->circle->color);
@@ -84,14 +126,7 @@ head->circle = newCricle((Color){170,170,170,255},4,0,GetScreenWidth()/2,GetScre
             current = current->next;
         }
         if(IsKeyPressed(KEY_Q)){
-            int r = (int)(((float)rand()/(float)(RAND_MAX)) * 255);
-            int g = (int)(((float)rand()/(float)(RAND_MAX)) * 255);
-            int b = (int)(((float)rand()/(float)(RAND_MAX)) * 255);
-            float size = (((float)rand()/(float)(RAND_MAX)) * 20.f);
-            float length = (((float)rand()/(float)(RAND_MAX)) * 10.f + 4);
-            float speed = (((float)rand()/(float)(RAND_MAX)) * 20.f + 3);
-
-            appEnd(&tail,newCricle((Color){r,g,b,255},size,length,tail->circle->x,tail->circle->y,speed));
+            addRandCircle(&tail);
         }
         if(IsKeyPressed(KEY_W)){
             delEnd(&tail);
@@ -137,4 +172,15 @@ void appEnd(node **tail_t, Circle* data){
     (*tail_t)->prev = prev;
     (*tail_t)->circle = data;
     (*tail_t)->next = NULL;
+}
+
+void addRandCircle(node** tail){
+    int r = (int)(((float)rand()/(float)(RAND_MAX)) * 255);
+            int g = (int)(((float)rand()/(float)(RAND_MAX)) * 255);
+            int b = (int)(((float)rand()/(float)(RAND_MAX)) * 255);
+            float size = (((float)rand()/(float)(RAND_MAX)) * 20.f);
+            float length = (((float)rand()/(float)(RAND_MAX)) * 10.f + 4.f);
+            float speed = (((float)rand()/(float)(RAND_MAX)) * 40.f -20.f);
+
+            appEnd(tail,newCricle((Color){r,g,b,255},size,length,(*tail)->circle->x,(*tail)->circle->y,speed));
 }
